9cf4494674c7513edb24747cea81d96235e91bf9
shivani sharma <shikha@shivanis-MacBook-Air.local>
2024-08-14 03:32:30 +0530

    added - carousel for project card and s3 bucket images

--- src/Components/Project/Project.tsx
+++ src/Components/Project/Project.tsx
@@ -1,54 +1,93 @@
 import React from 'react';
-import { Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Chip, IconButton, Typography } from "@mui/material";
-import LocalAtmIcon from "@mui/icons-material/LocalAtm";
-import projectsData from './projects.json';
-import { FaGithub} from 'react-icons/fa';
-import  OpenInNew from '@mui/icons-material/OpenInNew'
-import './Project.css'
+import { Box, Card, CardActions, CardContent, CardMedia, Chip, IconButton, Typography } from "@mui/material";
+import { FaGithub } from 'react-icons/fa';
+import OpenInNew from '@mui/icons-material/OpenInNew';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import projectsData from './Projects.json';
 
+import './Project.css';
 
-function Project() {
+interface Project {
+  _id: string;
+  name: string;
+  description: string;
+  github: string;
+  demo: string;
+  image: string[];
+  skills: string;
+}
+
+const Project = () => {
   return (
     <div className='project-container'>
-         <div className='project-header'>Projects</div>
+      <div className='project-header'>Projects</div>
+      <div className='project-sub-header'>Check out a selection of things I've built</div>
       {projectsData.map((project) => (
         <div className='col-md-6' key={project.name}>
-          <Card className='project-card' sx={{ display: 'flex', mb: 2 }}>
-            <CardMedia
-              component="img"
-              sx={{ width: 151 }}
-              image={project.image} 
-              alt={project.name}
-            />
-            <Box sx={{ display: 'flex', flexDirection: 'column', flex: '1 0 auto' }}>
+          <Card className='project-card' sx={{ p: 1 }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center', m: 2 }}>
+              <Carousel
+                showThumbs={false}
+                infiniteLoop
+                autoPlay = {true}
+                stopOnHover={true}
+                emulateTouch
+                swipeable
+              >
+                {project.image.map((image, index) => (
+                   <a href={image} target="_blank" rel="noopener noreferrer">
+                  <div key={index}>
+                   
+                      <CardMedia
+                        component="img"
+                        sx={{
+                          borderColor: '#9aa794 !important',
+                          border: 'solid 1px',
+                          borderRadius: '4px',
+                          maxWidth: '100%',
+                        }}
+                        image={image}
+                      />
+                  
+                  </div>
+                  </a>
+                ))}
+              </Carousel>
+            </Box>
+            <Box className='card-content-container' sx={{ display: 'flex', flexDirection: 'column', flex: '1 0 auto' }} >
+
               <CardContent>
-                <Typography id='proj-name' component="div" variant="h5">
-                  {project.name}
-                </Typography>
-                <Typography id='proj-desc' variant="body2" color="text.secondary" component="div" sx={{ mb: 2 }}>
-                  {project.description}
-                </Typography>
-                <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-                  {project.skills.split(',').map((skill) => (
-                    <Chip
-                      key={skill.trim()}
-                      icon={<LocalAtmIcon />}
-                      size="small"
-                      label={skill.trim()}
-                      color="success"
-                      sx={{ marginRight: 1, marginBottom: 1 }}
-                    />
-                  ))}
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                  <Typography id='proj-name' component="div" variant="h5">
+                    {project.name}
+                  </Typography>
+                  <Box sx={{ display: 'flex', flexWrap: 'wrap', ml: 2 }}>
+                    {project.skills.split(',').map((skill) => (
+                      <Chip
+                        className='skill-pill'
+                        key={skill.trim()}
+                        size="small"
+                        label={skill.trim()}
+                        variant='outlined'
+                        sx={{ marginRight: 1, padding: 1, color: '#40e0d0', borderColor: '#40e0d0', fontFamily: 'monospace' }}
+                      />
+                    ))}
+                  </Box>
                 </Box>
-              </CardContent>
+                <Typography id='proj-desc' variant="body2" color="text.secondary" component="div" sx={{ mt: 1, mb: 2, fontSize: '15px !important' }}>
+                <span dangerouslySetInnerHTML={{ __html: project.description }} />
+                </Typography>
+               
               <CardActions>
                 <IconButton className='action-btn' href={project.github} target="_blank" rel="noopener noreferrer">
-                  <FaGithub/>
+                  <FaGithub />
                 </IconButton>
                 <IconButton className='action-btn' href={project.demo} target="_blank" rel="noopener noreferrer">
-                <OpenInNew/>
+                  <OpenInNew />
                 </IconButton>
               </CardActions>
+               </CardContent>
             </Box>
           </Card>
         </div>
